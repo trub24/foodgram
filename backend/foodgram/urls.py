d909b034d41cfffaaf0076868aac9a8d4feb5189
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-08 05:31:50 +0300

    redoc add

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path(
+        'api/docs', TemplateView.as_view(template_name='docs/redoc.html'),
+        name='redoc'
+    )
 ]
