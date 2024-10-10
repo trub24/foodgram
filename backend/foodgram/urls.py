2c6f1676bd13f32955a324ebee8655c6fc67be2b
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-10 12:21:14 +0300

    Change urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
