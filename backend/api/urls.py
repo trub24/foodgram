9a41032cca475b768655d541a62ab547535ca963
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-10 04:52:14 +0300

    Redirection change

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('<uuid:query>/', views.redirect, name='redirect')
+    path('<int:query>/', views.redirect, name='redirect')
 ]
