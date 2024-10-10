b5283a564015b45a5a9178f83eda444ded8be4f7
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-10 15:02:32 +0300

    Small change

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponseRedirect
 def redirection(request, surl):
     id = short_url.decode_url(surl)
     return HttpResponseRedirect(
-        reverse('api:recipe-detail', kwargs={'recipe_id': id})
+        reverse('api:recipes-detail', kwargs={'pk': id})
     )
