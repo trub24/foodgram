613730ce46cdaf9fb7e46f921bf8f41af71491c9
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-10 06:05:42 +0300

    10

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ import short_url
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+
 def redirection(request, surl):
     id = short_url.decode_url(surl)
     return HttpResponseRedirect(reverse(
