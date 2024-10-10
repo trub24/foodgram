d35c1784a537edb5ca93971171128f4a0afc036b
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-10 06:15:02 +0300

    Some changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,11 @@
 import short_url
 from django.http import HttpResponseRedirect
-from django.urls import reverse
 
 
 def redirection(request, surl):
-    id = short_url.decode_url(surl)
-    return HttpResponseRedirect(reverse(
-        'api:recipes', kwargs={'recipe_id': id}
-    ))
+    redirect_url = 'https://{}/{}/{}'.format(
+        'foodgram-12.zapto.org',
+        'recipes',
+        short_url.decode_url(surl)
+    )
+    return HttpResponseRedirect(redirect_url)
