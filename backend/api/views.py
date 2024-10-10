e9112972bfc919c7bf43e6b366d244406250e935
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-10 06:04:52 +0300

    7

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,9 @@
 import short_url
-from django.http import HttpResponseRedirect 
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 def redirection(request, surl):
     id = short_url.decode_url(surl)
-    return HttpResponseRedirect(reversed(
+    return HttpResponseRedirect(reverse(
         'api:recipes', kwargs={'recipe_id': id}
     ))
