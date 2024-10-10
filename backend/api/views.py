6293c889ae485fd1017df1ec8c82f7470ebd0d6e
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-10 05:59:16 +0300

    1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,7 @@
 import short_url
-from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
-from recipe.models import Recipe
 
 
 def redirection(request, surl):
     id = short_url.decode_url(surl)
-    recipe = get_object_or_404(Recipe, id=id)
-    return redirect(recipe)
+    return redirect('api:recipes', kwargs={'recipe_id': id})
