1a364a62da7f9f21e8c75112025a0fa61fdb55e2
Юрий Трубников <trubnikov.yura@yandex.ru>
2024-10-10 05:13:28 +0300

    1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,10 @@
 import short_url
+from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
+from recipe.models import Recipe
 
 
-def redirection(request, query):
-    id = short_url.decode(query)
-    return redirect(f'https://foodgram-12.zapto.org/recipes/{id}')
+def redirection(request, surl):
+    id = short_url.decode(surl)
+    recipe = get_object_or_404(Recipe, id=id)
+    return redirect(f'https://foodgram-12.zapto.org/recipes/{recipe.id}')
